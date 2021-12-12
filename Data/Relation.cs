b5179fd0d1d731f7ca77c83afbdf33d429a7ab52
ulyan.yunakh <ulyan.yunakh@n-able.com>
2021-12-13 01:04:38 +0300

    class

--- Data/Relation.cs
+++ Data/Relation.cs
@@ -3,10 +3,10 @@ namespace computer_build_assistant_admin_app.Data
     public class Relation
     {
         public string FirstClassName { get; set; }
-        public string Class1Copy { get; set; }
+        public string FirstClassCopyName { get; set; }
         public int FirstClassId { get; set; }
         public string SecondClassName { get; set; }
-        public string Class2Copy { get; set; }
+        public string SecondClassCopyName { get; set; }
         public int SecondClassId { get; set; }
         public string Name { get; set; }
     }
